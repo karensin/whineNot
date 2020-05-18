8b29969fa5bc523d2d3e118dea8a2c962fa91a84
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-05-18 11:50:38 -0700

    Updates

---
+++ precache-manifest.11033be82d0818a92b7634811a0300f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699f776b578224fcfb521b353861878c",
+    "url": "/portfolio2019/whineNot/index.html"
+  },
+  {
+    "revision": "2afcb1f37b97d4bb18e2",
+    "url": "/portfolio2019/whineNot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d6b47515f42f50fc6b6",
+    "url": "/portfolio2019/whineNot/static/js/2.d94c4e8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio2019/whineNot/static/js/2.d94c4e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2afcb1f37b97d4bb18e2",
+    "url": "/portfolio2019/whineNot/static/js/main.c515ef09.chunk.js"
+  },
+  {
+    "revision": "d40d894e6011146df7c1",
+    "url": "/portfolio2019/whineNot/static/js/runtime-main.e9f1b0db.js"
+  }
+]);
