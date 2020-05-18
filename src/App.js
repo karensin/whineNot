e9e3ba28f9c1f2559505de45782eb2d4fb1772b5
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-05-18 11:52:03 -0700

    please work

--- src/App.js
+++ src/App.js
@@ -1,44 +1,66 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
+const axios = require('axios');
 
 function App() {
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
-
-  // const api_call = await fetch(``)
-  const domain = "https://api.globalwinescore.com/globalwinescores/latest/?wine_id=&vintage=2000&color=&is_primeurs=&lwin=1014033&lwin_11=10140332000&limit=100&offset=100&ordering=-date"
   const API_KEY = "7c242d580dd52bf8e0d0e49da4b923263630b26f"
 
+
+
   useEffect(() => {
-    console.log('did u run')
-    fetch(
-      domain, {
-      method: "GET",
-      headers: {
-        // Accept: "application/json",
-        'Content-Type': "application/json",
-        'Authorization': "Token 7c242d580dd52bf8e0d0e49da4b923263630b26f"
+
+    let domain = 'https://api.globalwinescore.com/globalwinescores/latest/?wine_id=&vintage=2000&color=&is_primeurs=&lwin=1014033&lwin_11=10140332000&limit=100&offset=100&ordering=-date';
+
+    async function getUser() {
+      try {
+        const response = await axios.get(domain, {
+          withCredentials: true,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Accept': "application/json",
+            'Content-Type': "application/json",
+            'Authorization': "Token 7c242d580dd52bf8e0d0e49da4b923263630b26f"
+          },
+        });
+        console.log(response)
+      }
+      catch (error) {
+        console.error(error);
       }
     }
-    )
-      .then(res => res.json())
-      .then(response => {
-        console.log(response)
-        // setCommitHistory(response.items);
-        setIsLoading(false);
-      })
-      .catch(error => console.log(error));
+    getUser()
+    // getUser()
+    // console.log('did u run')
+    // fetch(
+    //   domain, {
+    //   method: "GET",
+    //   headers: {
+    //     // Accept: "application/json",
+    //     'Content-Type': "application/json",
+    //     'Authorization': "Token 7c242d580dd52bf8e0d0e49da4b923263630b26f"
+    //   }
+    // }
+    // )
+    //   .then(res => res.json())
+    //   .then(response => {
+    //     console.log(response)
+    //     // setCommitHistory(response.items);
+    //     setIsLoading(false);
+    //   })
+    //   .catch(error => console.log(error));
   }, []);
   // async function requestData() {
   //   try {
   //     console.log('1')
-  //     const res = await fetch(domain, {
-  //       withCredentials: true,
-  //       headers: {
-  //         'Accept': "application/json",
-  //         'Content-Type': "application/json",
-  //         'Authorization': "Token 7c242d580dd52bf8e0d0e49da4b923263630b26f"
-  //       },
+  // const res = await fetch(domain, {
+  //   withCredentials: true,
+  //   headers: {
+  //     'Accept': "application/json",
+  //     'Content-Type': "application/json",
+  //     'Authorization': "Token 7c242d580dd52bf8e0d0e49da4b923263630b26f"
+  //   },
 
   //     });
 
